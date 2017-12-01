580e922ba77487323cae12198cf42fbfb3a9aa1c
Will Holt <williamholt@smu.edu>
2017-12-01 10:50:19 -0500

    Update SAS Code Part 2.R

--- SAS Code Part 2.R
+++ SAS Code Part 2.R
@@ -23,3 +23,27 @@ proc sgscatter data = DATASETNAME;
 matrix VARIABLE1 VARIABLE2 VARIABLEn;
 run; */
 
+/*Put everything in the model
+proc reg ;
+model LogSales = VARIABLE1 VARIABLE2 VARIABLEn;
+run;*/
+
+/*Check Cook's D visual to identify data that has high leverage. 
+Don't simply rely on the height of the bars, check the axis to determine the range*/
+
+/*If we need to delete an observation based on hat matrix value:
+data NEW DATANAME;
+set PREVIOUS DATASET;
+if _n_ = OBSERVATION NUMBER TO DELETE then delete;
+run;*/
+
+/*After deleting then re-run to check if variance inflation dropped
+proc reg data = NEW DATANAME;
+model logSales = VARIABLES;
+run;*/
+
+/*Variance inflation Factor VIF can help us understand collinearity*/
+
+/*Around the 14:00 mark of our video for 14.9 Dr. Magee talks about validating the data with another data set.
+We should follow this
+Train is used to build the model and test is used to test the model*/
